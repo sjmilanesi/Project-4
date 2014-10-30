e00e376c06c7fd8cd3d76aa901eb6f5108517a9b
Steven Milanesi <sjmilanesi@users.noreply.github.com>
2014-10-30 16:23:50 -0400

    Create TeamManager.java

---
+++ TeamManager.java
@@ -0,0 +1,32 @@
+public class TeamManager {
+
+	private Olympian[] olympians;
+	private Olympian extra = new Olympian();
+
+	public TeamManager(Olympian[] olympians) {
+		this.olympians = olympians;
+
+	}
+
+	public void getTeams() {
+
+		Olympian[] femaleOlympians = new Olympian[15];
+		Olympian[] maleOlympians = new Olympian[15];
+
+		int femaleCount = 0;
+		int maleCount = 0;
+
+		for (int i = 0; i < olympians.length; i++) {
+
+			if (olympians[i].getSex().equals(Sex.Female)) {
+				femaleOlympians[maleCount] = olympians[i];
+				femaleCount++;
+
+			} else if (olympians[i].getSex().equals(Sex.Male)) {
+				maleOlympians[femaleCount] = olympians[i];
+				maleCount++;
+
+			}
+		}
+	}
+}
