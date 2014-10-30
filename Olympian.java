fdb89092ca8d4d274ae6bcf453c3cb52001a1669
Steven Milanesi <sjmilanesi@users.noreply.github.com>
2014-10-30 16:03:40 -0400

    Create Olympian.java

---
+++ Olympian.java
@@ -0,0 +1,38 @@
+package Main.Olympian;
+
+
+public class Olympian {
+    private String name;
+    private Sex sex;
+    private int age;
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public void setSex(Sex sex)
+    {
+        this.sex = sex;
+    }
+
+    public void setAge(int age)
+    {
+        this.age = age;
+    }
+
+    public String getname()
+    {
+        return this.name;
+    }
+
+    public Sex getSex()
+    {
+        return this.sex;
+    }
+
+    public int getAge()
+    {
+        return this.age;
+    }
+}
