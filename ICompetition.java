0861a9217e57d52dcbb8f88bbc1996ab19b4847f
Steven Milanesi <sjmilanesi@users.noreply.github.com>
2014-11-20 13:06:35 -0500

    Create ICompetition.java

---
+++ ICompetition.java
@@ -0,0 +1,10 @@
+public class ICompetition {
+    public Event getEvent1();
+    public Team getTeam1();
+    public Team getTeam2();
+
+    public void setEvent(Event myEvent);
+    public void setTeam1(Team myTeam1);
+    public void setTeam2(Team myTeam2);
+
+}
